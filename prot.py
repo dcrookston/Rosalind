8c134b12d297dac59af34f11415cc1abf47537b2
Dan <dcrookston@gmail.com>
2021-01-29 19:30:40 -0700

    Create prot.py
    Completed solution to "Translating RNA into Protein"

---
+++ prot.py
@@ -0,0 +1,47 @@
+#A solution to the problem "Translating RNA into Protein"
+#http://rosalind.info/problems/prot/
+
+import re
+
+amino_translator = {
+    "UUU": "F", "UUC": "F", "UUA": "L",
+    "UUG": "L", "UCU": "S", "UCC": "S",
+    "UCA": "S", "UCG": "S", "UAU": "Y",
+    "UAC": "Y", "UAA": "-", "UAG": "-",
+    "UGU": "C", "UGC": "C", "UGA": "-",
+    "UGG": "W", "CUU": "L", "CUC": "L",
+    "CUA": "L", "CUG": "L", "CCU": "P",
+    "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAU": "H", "CAC": "H", "CAA": "Q",
+    "CAG": "Q", "CGU": "R", "CGC": "R",
+    "CGA": "R", "CGG": "R", "AUU": "I",
+    "AUC": "I", "AUA": "I", "AUG": "M",
+    "ACU": "T", "ACC": "T", "ACA": "T",
+    "ACG": "T", "AAU": "N", "AAC": "N",
+    "AAA": "K", "AAG": "K", "AGU": "S",
+    "AGC": "S", "AGA": "R", "AGG": "R",
+    "GUU": "V", "GUC": "V", "GUA": "V",
+    "GUG": "V", "GCU": "A", "GCC": "A",
+    "GCA": "A", "GCG": "A", "GAU": "D",
+    "GAC": "D", "GAA": "E", "GAG": "E",
+    "GGU": "G", "GGC": "G", "GGA": "G",
+    "GGG": "G"}
+
+def prot(bases):
+    triplet = ""
+    protein = ""
+    
+    if (re.match("^[ACUG]+$", bases) is None):
+        raise ValueError("'bases' must consist only of A, U, C, G codons.")
+    for base in bases:
+        triplet += base
+
+        if(len(triplet) == 3):
+            if (amino_translator[triplet] == "-"):
+                return protein
+            
+            protein += amino_translator[triplet]
+            triplet = ""
+
+    return protein
+        
