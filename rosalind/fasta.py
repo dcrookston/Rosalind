3cedb90ddfb3e8fd6c14e3ca0f0e3f202b3f5987
Dan <dcrookston@gmail.com>
2021-01-30 15:47:00 -0700

    Update fasta.py
    added comments

--- rosalind/fasta.py
+++ rosalind/fasta.py
@@ -12,9 +12,16 @@ def generead(file):
 
         #Create a new gene
         if (re.match("^>", line) is not None):
+            #Split the line at whitespace, skipping the first > character
             words = re.split("[\s]+", line[1:])
+
+            #Create a new gene instance to hold this gene
             this_gene = gene(words[0])
+
+            #Put it in the accumulator
             accumulator.append(this_gene)
+
+        #Add base pairs to the gene    
         if (re.match("^[ACTGURYKMSWBDHVN-]+$", line) is not None):
             this_gene.add_bases(line)
 
