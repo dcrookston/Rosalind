342b91bbf727157f9f018bef2255d20fc78622e8
Dan <dcrookston@gmail.com>
2021-01-30 15:46:48 -0700

    Update GC.py
    Fixed #4

--- rosalind/GC.py
+++ rosalind/GC.py
@@ -5,36 +5,20 @@ import re
 import fileinput
 import sys
 import rosalind.Gene
+import rosalind.fasta
 
 #A variable to hold the winner so far.
-winner = Gene.gene("")
+winner = rosalind.Gene.gene("")
 
-#A temp variable to hold the gene currently being processed
-this_gene = Gene.gene("")
-
-if (len(sys.argv) == 1):
+if (len(sys.argv) > 1):
+    gene_list = rosalind.fasta.generead(sys.argv[1])
+else:
     print("Enter dataset (EOF to submit):")
+    gene_list = rosalind.fasta.generead(None)
 
-#Read in copy-pasted data
-for line in fileinput.input():
-
-    line = line.strip()
-    #This line is a label
-    if (re.match("^>[\s]*Rosalind_[\d]", line) is not None):
-
-        if (this_gene.percent_gc() > winner.percent_gc()):
-            winner = this_gene
-            
-        #Replace old this_gene with our new gene
-        this_gene = Gene.gene(line[1:])
-
-    #This line is a string of bases
-    elif (re.match("^[ACTG]+$", line) is not None):
-        #Update the gene with the current list of bases
-        this_gene.add_bases(line)
-
-if (this_gene.percent_gc() > winner.percent_gc()):
-    winner = this_gene
+for i in gene_list:
+    if (i.percent_gc() > winner.percent_gc()):
+        winner = i
 
 print(winner)
 print(winner.percent_gc())
