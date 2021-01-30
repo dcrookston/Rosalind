de73d28218cb6b09af80fa15d0d277e1938b74ad
Dan <dcrookston@gmail.com>
2021-01-30 15:47:44 -0700

    Create generead_test.py
    renamed a test file

---
+++ tests/generead_test.py
@@ -0,0 +1,17 @@
+import pickle
+from rosalind.Gene import gene
+from rosalind.fasta import generead
+import pytest
+
+def test_generead():
+    fp = open("genes.p", "rb")
+    pickled_genes = pickle.load(fp)
+    fp.close()
+
+    genes = generead("rosalind/FASTA.txt")
+    for i in zip(pickled_genes, genes):
+        assert(i[0].bases == i[1].bases)
+
+def test_generead_nofile():
+    with pytest.raises(FileNotFoundError):
+        generead("nonexistent.txt")
