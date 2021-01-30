e4807b7276d108969eb6d647b27391c7daa01423
Dan <dcrookston@gmail.com>
2021-01-29 23:48:30 -0700

    Update fasta.py
    Preliminary refinements to FASTA reader

--- fasta.py
+++ fasta.py
@@ -3,18 +3,21 @@
 import re
 import fileinput
 import sys
-import Gene
+from Gene import gene
 
-def generead(file, accumulator):
+def generead(file):
+    accumulator = []
     for line in fileinput.input(file):
         line = line.strip()
 
         #Create a new gene
         if (re.match("^>", line) is not None):
             words = re.split("[\s]+", line[1:])
-            gene = Gene(words[0])
-            accumulator.append(gene)
-        if (re.match("^[ACTGURYKMSWBDHVN-]$", line) is not None):
-            gene.add_bases(line)
+            this_gene = gene(words[0])
+            accumulator.append(this_gene)
+        if (re.match("^[ACTGURYKMSWBDHVN-]+$", line) is not None):
+            this_gene.add_bases(line)
+
+    fileinput.close()
 
     return accumulator
